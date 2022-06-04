0ec2b65e49dbdb13ee61c7670f3d95af302a7b96
pazguille <guille87paz@gmail.com>
2022-06-04 02:53:09 -0300

    Initial commit.

---
+++ pages/search.js
@@ -0,0 +1,9 @@
+export default function Search() {
+  return (
+    <div className="results list page page-on">
+    <div className="results-content page-content">
+      <h2>Resultados</h2>
+    </div>
+  </div>
+  )
+}
